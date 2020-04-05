6c01ca0477399a2a07bc36a694850ad3c3c40228
kynmh69 <hiroki.kayanuma@gmail.com>
2020-04-05 11:58:29 +0900

    add gui.

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,17 +1,25 @@
-from tkinter import Button, Entry, Tk, Menubutton, Label, TOP, BOTTOM
+from configparser import ConfigParser
+from pathlib import Path
+from tkinter import Button, Entry, Tk, Menubutton, Label, TOP, BOTTOM, RAISED, Menu, IntVar
 from typing import Union
 
+from cotoha_api.cotoha_api import CotohaApi
 from logger.logger import LoggerUtils
 
 
 class Application:
-    def __init__(self, master=None):
+    def __init__(self):
         self.__master: Union[None, Tk] = None
         self.__logger = LoggerUtils.get_instance()
         self.__sentence_entry: Union[None, Entry] = None
         self.__result_entry: Union[None, Entry] = None
         self.__submit_button: Union[None, Button] = None
         self.__menu_button: Union[None, Menubutton] = None
+        self.__vertical_px = 1080
+        self.__horizontal_px = 1920
+        self.__window_title = 'Cotoha API'
+        self.__setting = ConfigParser()
+        self.__cotoha_api = CotohaApi()
         return
 
     @property
@@ -22,14 +30,38 @@ class Application:
         """
         return self.__master
 
+    @property
+    def vertical_px(self):
+        """
+
+        :return:
+        """
+        return self.__vertical_px
+
+    @property
+    def horizontal_px(self):
+        """
+
+        :return:
+        """
+        return self.__horizontal_px
+
+    @property
+    def window_title(self):
+        """
+
+        :return:
+        """
+        return self.__window_title
+
     def create_window(self) -> None:
         """
         Create window.
         :return:
         """
         self.__master = Tk()
-        self.__master.title('Cotoha API')
-        self.__master.geometry('1920x1080')
+        self.__master.title(self.window_title)
+        self.__master.geometry(f'{self.horizontal_px}x{self.vertical_px}')
         return
 
     def create_sentence_form(self) -> None:
@@ -40,5 +72,26 @@ class Application:
         label = Label(self.__master, text='Sentence')
         label.pack(side=TOP)
         self.__sentence_entry = Entry(self.__master, bd=2)
-        self.__sentence_entry.pack(BOTTOM)
+        self.__sentence_entry.pack(side=BOTTOM)
+        return
+
+    def create_pull_down_menu(self):
+        mb = Menubutton(self.master, text='Select API.', relief=RAISED)
+        mb.grid()
+        mb.menu = Menu(mb, tearoff=0)
+        mb['menu'] = mb.menu
+        var1 = IntVar()
+        var2 = IntVar()
+        var3 = IntVar()
+
+        mb.menu.add_checkbutton(label="")
+        return
+
+    def read_config(self):
+        path = Path('src/setting/setting.conf')
+        if path.exists():
+            raise FileExistsError
+
+        self.__setting.read(path)
+
         return
--- src/test.py
+++ src/test.py
@@ -11,5 +11,8 @@ if __name__ == "__main__":
     logger_initialize()
     logger = LoggerUtils.get_instance()
     logger.info(f"{EQUAL_STR} START {EQUAL_STR}")
-
+    app = Application()
+    app.create_window()
+    app.create_sentence_form()
+    app.master.mainloop()
     logger.info(f"{EQUAL_STR} END {EQUAL_STR}")
